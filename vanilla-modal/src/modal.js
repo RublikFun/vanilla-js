0e3fd663de788193603a004357aafa1f4c643c0e
Rublivsky Vitaly <v.rublivsky@daviann.com>
2017-11-14 16:29:10 +0200

    fix vanilla modal

--- vanilla-modal/src/modal.js
+++ vanilla-modal/src/modal.js
@@ -1,34 +1,28 @@
 let buttons = document.querySelectorAll('[data-modal-btn]'),
-    modals = document.querySelectorAll('[data-modal]');
+  modals = document.querySelectorAll('[data-modal]');
 
-    //Open modal window
-    buttons.forEach((btn) => {
-        btn.onclick = function(event) {
-            let target = event.target;
-            let modalID = target.dataset.modalBtn;
+// Open modal window
+buttons.forEach((btn) => {
+  btn.onclick = function (event) {
+    let target = event.target;
+    let modalID = target.dataset.modalBtn;
 
-            modals.forEach((modal) => {
-                if (modalID == modal.dataset.modal) {
-                    modal.classList.add('open');
-                }
-            });
-        }
-    });
-
-    // Close modal window on click Close icon
     modals.forEach((modal) => {
-        let close = modal.querySelectorAll('.modal__close')[0];
-        console.log(close);
-            close.onclick = function () {
-                modal.classList.remove('open');
-            }
-    });
+      if (modalID == modal.dataset.modal) modal.classList.add('open');      
+    })
+  }
+});
+
+// Close modal window on click Close icon
+modals.forEach((modal) => {
+  let close = modal.querySelectorAll('.modal__close')[0];
+
+  close.onclick = () => modal.classList.remove('open');
+});
 
 // Close modal window on click outside
-window.onclick = function(event) {
-    modals.forEach((modal) => {
-        if (event.target == modal) {
-            modal.classList.remove('open');
-        }
-    });
+window.onclick = function (event) {
+  modals.forEach((modal) => {
+    if (event.target == modal) modal.classList.remove('open');
+  });
 }
